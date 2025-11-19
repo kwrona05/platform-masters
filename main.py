02ea5e99c0cebe1aeba3e5fe28d65b9d6c7b4fa2
kwrona05 <kwrona05@gmail.com>
2025-11-19 08:29:41 +0100

    commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .auth import router as auth_router
+from auth import router as auth_router
 from .database import create_db_and_tables
 
 app = FastAPI(title="FastAPI Auth Backend")
